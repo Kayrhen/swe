96e46f6cfa1b453d4f5bde9598fa5dfd7b7bda84
Kayrhen <karen.ding@yahoo.com>
2019-09-28 16:54:58 -0500

    9 files added, 3 deleted

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from "./Pages/Home.js";
+
+class App extends Component {
+  render() {
+    return <Home />;
+  }
+}
+export default App;
